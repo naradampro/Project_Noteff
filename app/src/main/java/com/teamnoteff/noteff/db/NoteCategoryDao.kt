45c094366a803d981fade523204ddcb27fc33225
Narada Abeykoon <naradampro@gmail.com>
2022-01-01 21:38:24 +0530

    repositories and DAOs added
    - repository package added.
    - DAOs included in db package

---
+++ app/src/main/java/com/teamnoteff/noteff/db/NoteCategoryDao.kt
@@ -0,0 +1,24 @@
+package com.teamnoteff.noteff.db
+
+import androidx.room.*
+import com.teamnoteff.noteff.entities.NoteCategory
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteCategoryDao {
+
+    @Insert
+    suspend fun insertCategory(category: NoteCategory):Long
+
+    @Update
+    suspend fun updateCategory(category: NoteCategory):Int
+
+    @Delete
+    suspend fun deleteCategory(category: NoteCategory):Int
+
+    @Query("DELETE FROM category_table")
+    suspend fun deleteAll():Int
+
+    @Query("SELECT * FROM category_table")
+    fun getAllCategories():Flow<List<NoteCategory>>
+}
