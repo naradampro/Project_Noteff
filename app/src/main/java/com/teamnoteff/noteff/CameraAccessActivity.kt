dee67653f152164a551d04c5f5ca7e73e51c7d01
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-01-05 20:03:29 +0530

    Added Camera Access Activity

---
+++ app/src/main/java/com/teamnoteff/noteff/CameraAccessActivity.kt
@@ -0,0 +1,52 @@
+package com.teamnoteff.noteff
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.github.dhaval2404.imagepicker.ImagePicker
+
+class CameraAccessActivity: AppCompatActivity() {
+
+    var imagePicker: ImageView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imagePicker = findViewById(R.id.picker_image)
+
+        val gallery = findViewById<Button>(R.id.gallery)
+        val camera = findViewById<Button>(R.id.camera)
+
+        gallery.setOnClickListener {
+
+            ImagePicker.with(this).galleryOnly().galleryMimeTypes(arrayOf("image/*")).crop()
+                .maxResultSize(400, 400).start()
+
+        }
+
+        camera.setOnClickListener {
+
+            ImagePicker.with(this).cameraOnly().crop().maxResultSize(400, 400).start()
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode== Activity.RESULT_OK && requestCode== ImagePicker.REQUEST_CODE) {
+
+
+            imagePicker?.setImageURI(data?.data)
+
+        }
+
+    }
+
+
+}
