008a99df5049c5a9a50acb10adceef6fb8ee8c9a
Narada Abeykoon <naradampro@gmail.com>
2022-01-14 21:03:42 +0530

    View all notes in NotesFragment connected with databse

--- app/src/main/java/com/teamnoteff/noteff/ui/common/NotesViewModel.kt
+++ app/src/main/java/com/teamnoteff/noteff/ui/common/NotesViewModel.kt
@@ -1,7 +1,12 @@
 package com.teamnoteff.noteff.ui.common
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.teamnoteff.noteff.entities.Note
+import com.teamnoteff.noteff.repositories.NoteRepository
 
-class NotesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class NotesViewModel(val noteRepository: NoteRepository) : ViewModel() {
+    val allNotes: LiveData<List<Note>>
+        get() = noteRepository.notes.asLiveData()
 }
