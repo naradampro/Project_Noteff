e328d3a431ea56065ac41b7725a8f4c5addfc69e
Narada Abeykoon <naradampro@gmail.com>
2022-01-15 21:36:12 +0530

    View recent categories connected with the DB

--- app/src/main/java/com/teamnoteff/noteff/ui/common/HomeFragment.kt
+++ app/src/main/java/com/teamnoteff/noteff/ui/common/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.teamnoteff.noteff.ui.common
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Layout
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,16 +13,23 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.teamnoteff.noteff.R
+import com.teamnoteff.noteff.R.layout.categories_fragment
 import com.teamnoteff.noteff.databinding.HomeFragmentBinding
 import com.teamnoteff.noteff.db.NoteffDatabase
 import com.teamnoteff.noteff.entities.Note
+import com.teamnoteff.noteff.repositories.CategoryRepository
 import com.teamnoteff.noteff.repositories.NoteRepository
 import com.teamnoteff.noteff.ui.create.CreateNoteActivity
 import com.teamnoteff.noteff.ui.recycler_adapters.HomeNoteListAdapter
+import com.teamnoteff.noteff.ui.recycler_adapters.HomeRecentCategoriesRecyclerAdapter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+
 
 class HomeFragment : Fragment() {
 
@@ -32,13 +40,15 @@ class HomeFragment : Fragment() {
     private lateinit var binding: HomeFragmentBinding
     private lateinit var viewModel: HomeViewModel
     private lateinit var noteadapter: HomeNoteListAdapter
+    private lateinit var categoryadapter: HomeRecentCategoriesRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        binding=DataBindingUtil.inflate(inflater,R.layout.home_fragment,container,false)
+        binding=DataBindingUtil.inflate(inflater,
+            com.teamnoteff.noteff.R.layout.home_fragment,container,false)
 
         val btnAddNote: ImageButton = binding.floatingActionButton
 
@@ -47,15 +57,24 @@ class HomeFragment : Fragment() {
             startActivity(intent)
         }
 
+        binding.btnViewAllCategories.setOnClickListener{
+            findNavController().navigate(R.id.action_navigation_home_to_categories)
+        }
+
+        binding.btnViewAllNotes.setOnClickListener{
+            findNavController().navigate(R.id.action_navigation_home_to_notes)
+        }
+
         return binding.root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //createSampleNotes()
-        val dao = NoteffDatabase.getInstance(requireActivity().application).noteDao
+        val noteDao = NoteffDatabase.getInstance(requireActivity().application).noteDao
+        val categoryDao = NoteffDatabase.getInstance(requireActivity().application).noteCategoryDao
 
-        val factory = HomeViewModelFactory(NoteRepository(dao))
+        val factory = HomeViewModelFactory(NoteRepository(noteDao), CategoryRepository(categoryDao))
 
         viewModel = ViewModelProvider(this,factory)[HomeViewModel::class.java]
     }
@@ -63,6 +82,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         noteadapter = HomeNoteListAdapter()
+        categoryadapter = HomeRecentCategoriesRecyclerAdapter()
         initRecentNotesList()
         //testNoteQuery()
     }
@@ -77,7 +97,22 @@ class HomeFragment : Fragment() {
             this.adapter = noteadapter
         }
 
+        binding.recentCategoryList.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = GridLayoutManager(context,2)
+            // set the custom adapter to the RecyclerView
+            this.adapter = categoryadapter
+        }
+
         displayRecentNotesList()
+
+        viewModel.allCategories.observe(viewLifecycleOwner, {
+            categoryadapter.setList(it)
+            categoryadapter.notifyDataSetChanged()
+        })
+
+
 
         //Checking Recycler Adapter with sample note objects
         /*val sampleNotes: List<Note> = listOf(
--- app/src/main/java/com/teamnoteff/noteff/ui/common/HomeViewModelFactory.kt
+++ app/src/main/java/com/teamnoteff/noteff/ui/common/HomeViewModelFactory.kt
@@ -2,14 +2,16 @@ package com.teamnoteff.noteff.ui.common
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.teamnoteff.noteff.repositories.CategoryRepository
 import com.teamnoteff.noteff.repositories.NoteRepository
 
 class HomeViewModelFactory(
-    private val noteRepository: NoteRepository
+    private val noteRepository: NoteRepository,
+    private val categoryRepository: CategoryRepository
     ): ViewModelProvider.Factory{
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(HomeViewModel::class.java)){
-            return HomeViewModel(noteRepository) as T
+            return HomeViewModel(noteRepository,categoryRepository) as T
         }
         throw IllegalArgumentException("Unknown View Model class")
     }
