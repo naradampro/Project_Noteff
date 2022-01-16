0ddc8eab85f805eebf68732818b8da029da988df
Narada Abeykoon <naradampro@gmail.com>
2022-01-16 16:27:29 +0530

    Update ImageDataSegment.kt

--- app/src/main/java/com/teamnoteff/noteff/entities/ImageDataSegment.kt
+++ app/src/main/java/com/teamnoteff/noteff/entities/ImageDataSegment.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "plaintext_data_segment_table")
+@Entity(tableName = "image_data_segment_table")
 class ImageDataSegment(
     @PrimaryKey(autoGenerate = true)
     override val id: Int,
