671c1f6cc462884733583a569bd217c6e82f87d6
Narada Abeykoon <naradampro@gmail.com>
2022-01-16 16:25:12 +0530

    Missing data segment entity classes added.

---
+++ app/src/main/java/com/teamnoteff/noteff/entities/ImageDataSegment.kt
@@ -0,0 +1,17 @@
+package com.teamnoteff.noteff.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "plaintext_data_segment_table")
+class ImageDataSegment(
+    @PrimaryKey(autoGenerate = true)
+    override val id: Int,
+
+    @ColumnInfo(name = "note_id")
+    override val noteId: Int,
+
+    @ColumnInfo(name = "content")
+    val content:String
+) : DataSegment
