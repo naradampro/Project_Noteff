b4246fb7804a7a1160ad78c8a813f2a0c8702c7f
waokwarachchi <82043184+waokwarachchi@users.noreply.github.com>
2022-01-28 10:20:34 +0530

    Dao
    phoneNumberSegmentDao and PlainTextDataSegmentDao

---
+++ app/src/main/java/com/teamnoteff/noteff/db/PlainTextDataSegmentDao.kt
@@ -0,0 +1,24 @@
+package com.teamnoteff.noteff.db
+
+import androidx.room.*
+import com.teamnoteff.noteff.entities.PlainTextDataSegment
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PlainTextDataSegmentDao {
+
+    @Insert
+    suspend fun insertPlainTextDataSegment(plainTextDataSegment: PlainTextDataSegment):Long
+
+    @Update
+    suspend fun updatePlainTextDataSegment(plainTextDataSegment: PlainTextDataSegment):Int
+
+    @Delete
+    suspend fun deletePlainTextDataSegment(plainTextDataSegment: PlainTextDataSegment):Int
+
+    @Query("DELETE FROM plaintext_data_segment_table")
+    suspend fun deleteAll():Int
+
+    @Query("SELECT * FROM plaintext_data_segment_table")
+    fun getAllPlainTextDataSegments():Flow<List<PlainTextDataSegment>>
+}
