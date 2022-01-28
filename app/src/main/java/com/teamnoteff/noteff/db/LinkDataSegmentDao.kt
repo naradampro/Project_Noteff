0fae8c313423430ede128d479e416a1ccc5c35f3
waokwarachchi <82043184+waokwarachchi@users.noreply.github.com>
2022-01-28 09:39:05 +0530

    Create LinkDataSegmentDao.kt
    LinkDataSegmentDao

---
+++ app/src/main/java/com/teamnoteff/noteff/db/LinkDataSegmentDao.kt
@@ -0,0 +1,23 @@
+package com.teamnoteff.noteff.db
+
+import androidx.room.*
+import com.teamnoteff.noteff.entities.LinkDataSegment
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface LinkDataSegmentDao {
+    @Insert
+    suspend fun insertLinkDataSegment(linkDataSegment: LinkDataSegment):Long
+
+    @Update
+    suspend fun updateLinkDataSegment(linkDataSegment: LinkDataSegment):Int
+
+    @Delete
+    suspend fun deleteLinkDataSegment(linkDataSegment: LinkDataSegment):Int
+
+    @Query("DELETE FROM link_data_segment_table")
+    suspend fun deleteAll():Int
+
+    @Query("SELECT*FROM link_data_segment_table")
+    fun getAllLinkDataSegments():Flow<List<LinkDataSegment>>
+}
