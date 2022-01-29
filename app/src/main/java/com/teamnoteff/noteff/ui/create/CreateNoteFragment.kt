b6f8cf956542ddc08a4a8053e6d25074afc88497
Narada Abeykoon <amnmabeykoon@students.nsbm.ac.lk>
2022-01-29 18:35:43 +0530

    Category selection toggle fixed.

--- app/src/main/java/com/teamnoteff/noteff/ui/create/CreateNoteFragment.kt
+++ app/src/main/java/com/teamnoteff/noteff/ui/create/CreateNoteFragment.kt
@@ -2,10 +2,14 @@ package com.teamnoteff.noteff.ui.create
 
 import android.annotation.SuppressLint
 import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
@@ -34,6 +38,37 @@ class CreateNoteFragment : Fragment() {
     ): View {
         binding= CreateNoteFragmentBinding.inflate(inflater,container,false)
 
+
+        //Toggle group logics to select the note category
+        binding.toggleButton.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.btnExisting ->  {
+                        binding.textInputLayout.visibility = View.VISIBLE
+                        binding.txtUncategorized.visibility = View.GONE
+                        binding.txtLayoutNewCategory.visibility = View.GONE
+                    }
+
+                    R.id.btnNew -> {
+                        binding.txtUncategorized.visibility = View.GONE
+                        binding.textInputLayout.visibility = View.GONE
+                        binding.txtLayoutNewCategory.visibility = View.VISIBLE
+                    }
+
+                    R.id.btnUncategorized -> {
+                        binding.textInputLayout.visibility = View.GONE
+                        //binding.txtUncategorized.visibility = View.VISIBLE
+                        binding.txtLayoutNewCategory.visibility = View.GONE
+                    }
+                }
+            } else {
+                if (group.checkedButtonId == View.NO_ID) {
+                    binding.etNoteTitle.setBackgroundColor(Color.WHITE)
+                }
+            }
+        }
+
+        //opens data segments types list fragments
         binding.btnAddDS.setOnClickListener{
             Navigation.findNavController(binding.root).navigate(R.id.action_createNoteFragment_to_dataSegmentFragment)
         }
@@ -76,9 +111,9 @@ class CreateNoteFragment : Fragment() {
 
     @SuppressLint("NotifyDataSetChanged")
     fun displayDataSegmentList() {
-        mainViewModel.datasegments.observe(viewLifecycleOwner, {
+        mainViewModel.datasegments.observe(viewLifecycleOwner) {
             dsAdapter.setList(it)
-        })
+        }
 
         /*
         val sampleDSList: ArrayList<DataSegment> = arrayListOf(
