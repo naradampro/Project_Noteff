a827570062903314ec0f8dfb0834d145aedb9bec
Narada Abeykoon <amnmabeykoon@students.nsbm.ac.lk>
2022-02-13 23:19:56 +0530

    App setting menu layout errors fixed.

--- app/src/main/java/com/teamnoteff/noteff/ui/common/AppSettingsFragment.kt
+++ app/src/main/java/com/teamnoteff/noteff/ui/common/AppSettingsFragment.kt
@@ -1,21 +1,53 @@
 package com.teamnoteff.noteff.ui.common
 
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.setContentView
 import com.teamnoteff.noteff.R
+import com.teamnoteff.noteff.databinding.AboutUsFragmentBinding
+import com.teamnoteff.noteff.databinding.AppSettingsFragmentBinding
+import com.teamnoteff.noteff.ui.create.CreateNoteFragment
 
-class AppSettingsFragment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class AppSettingsFragment : Fragment() {
+    companion object {
+        fun newInstance() = CreateNoteFragment()
+    }
 
+    private lateinit var binding: AppSettingsFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding= AppSettingsFragmentBinding.inflate(inflater,container,false)
+
+        binding.alertButton.setOnClickListener {
+            showResetAlert()
+        }
+
+        return binding.root
     }
-    fun showAlert(view: View) {
-        Dialog().show(supportFragmentManager, "dialog")
+
+    private fun showResetAlert(){
+        val alertBuilder = AlertDialog.Builder(context)
+
+        alertBuilder.setTitle("Are you sure ?")
+        alertBuilder.setMessage("Your account and all the data will be permanently deleted.Are you sure want to continue?")
+
+        alertBuilder.setPositiveButton("Accept", DialogInterface.OnClickListener{ dialog, id ->
+            Log.d("dialoglog", "Accept Pressed")
+        }).setNegativeButton("Decline", DialogInterface.OnClickListener{ dialog, id ->
+            Log.d("dialoglog", "Decline Pressed")
+        })
+        alertBuilder.create().show()
     }
+
 }
